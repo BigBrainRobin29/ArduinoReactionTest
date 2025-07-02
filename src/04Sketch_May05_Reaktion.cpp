d3633441ef4e79c86dee67b4bedebb11cbfd3dd8
BigBrainRobin29 <152532785+BigBrainRobin29@users.noreply.github.com>
2025-07-02 12:26:30 +0200

    refactor: add more comments - <https://github.com/BigBrainRobin29/ArduinoReactionTest/issues/2>

--- src/04Sketch_May05_Reaktion.cpp
+++ src/04Sketch_May05_Reaktion.cpp
@@ -1,6 +1,7 @@
 #include "Arduino.h"
 #include "LiquidCrystal_I2C.h"
 
+// Method declarations
 long check();
 void printLcd();
 
@@ -47,28 +48,39 @@ void printLcd(String str) {
 }
 
 void setup() {
+  // Initialize LCD
   lcd.init();
   lcd.backlight();
 
+  // Initialize LED and button
   pinMode(LED, OUTPUT);
   pinMode(BUTTON, INPUT_PULLUP);
 
+  // Initialize random seed
+  randomSeed(analogRead(A0));
+
+  // Initialize Serial
   Serial.begin(9600);
   Serial.println("init");
 }
 
 void loop() {
+  // Arrays to store reaction times
   long times1[5];
   long times2[5];
 
+  // Run test 5 times with glasses on
   for (int i = 0; i < 5; i++) {
+    // Get response time
     long time = check();
 
+    // Button was pressed too early
     if (time == -1) {
       i--;
       continue;
     }
 
+    // Store the time
     times1[i] = time;
   }
 
@@ -76,14 +88,18 @@ void loop() {
 
   delay(2000);
 
+  // Run test 5 times without glasses
   for (int i = 0; i < 5; i++) {
+    // Get response time
     long time = check();
 
+    // Button was pressed too early
     if (time == -1) {
       i--;
       continue;
     }
 
+    // Store the time
     times2[i] = time;
   }
 
@@ -98,10 +114,13 @@ void loop() {
 
 long check() {
   printLcd("Press button\nto start!");
+
+  // Wait for button to be pressed
   while (digitalRead(BUTTON) == 1) {
     // pass
   } 
 
+  // Countdown before starting
   for (int i = 3; i >= 0; i--) {
     printLcd("Starting in " + String(i) + "...");
     delay(1000);
@@ -109,9 +128,11 @@ long check() {
 
   printLcd("Waiting...");
 
+  // Generate random delay between MIN_DELAY and MAX_DELAY
   int delayms = random(MIN_DELAY, MAX_DELAY);
   Serial.println(delayms);
 
+  // Wait for a random time while checking if the button is pressed to early
   for (int i = 0; i < delayms / 10; i++) {
     Serial.println(i);
     delay(10);
@@ -119,6 +140,8 @@ long check() {
     if (digitalRead(BUTTON) == 0) {
       printLcd("Too early!");
       delay(1000);
+
+      // Return -1 to indicate that the button was pressed too early
       return -1L;
     }
   }
@@ -129,10 +152,12 @@ long check() {
   long startTime = millis();
   long endTime;
 
+  // Wait for button to be pressed
   while (digitalRead(BUTTON) == 0) {
     // pass
   }
 
+  // Button was pressed, wait for release
   while (true) {
     int digital = digitalRead(BUTTON);
     Serial.println(digital);
@@ -142,6 +167,7 @@ long check() {
     }
   }
 
+  // Calculate the reaction time
   long diff = endTime - startTime;
   Serial.println(diff);
 
